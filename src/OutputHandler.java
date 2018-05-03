7bff8a149099c823e40d4fe0c3c4cf82a861b8ca
simon5521 <simon5521@freemail.hu>
2018-05-03 19:34:08 +0200

    elso sql funkciok
    hamarosan jon a tobbi is

--- src/OutputHandler.java
+++ src/OutputHandler.java
@@ -8,9 +8,22 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import java.sql.*;
+
+
 public class OutputHandler {
 
     final static Logger logger = Logger.getLogger(OutputHandler.class);
+    
+   //database variables 
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/EMP";
+
+   //  Database credentials
+   static final String USER = "gateway";
+   static final String PASS = "new_password";
+
 
     public OutputHandler() {
 
@@ -90,4 +103,52 @@ public class OutputHandler {
         }
 
     }
+    
+      public void clientSparkSend(AccData input) {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            
+          Class.forName("com.mysql.jdbc.Driver");
+          conn = DriverManager.getConnection(DB_URL,USER,PASS);
+          
+          stmt = conn.createStatement();
+          String sql;
+          sql = "insert into trainsensordata(AccX,AccY,AccZ,MicroTimestamp,SensorName) values("+String(input.AccX)+","+String(input.AccY)+","+String(input.AccZ)+","+String(input.MicroTimestamp)+",'train_test_1')";
+         
+          ResultSet rs = stmt.executeQuery(sql);
+
+          
+          rs.close();
+          stmt.close();
+          conn.close();
+
+
+
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            logger.error("there is an error with sql");
+        } catch (Exception e) {
+            logger.error("there is an error with accdata");
+            return;
+        }finally{
+        //finally block used to close resources
+        try{
+            if(stmt!=null)
+            stmt.close();
+        }catch(SQLException se2){
+        }// nothing we can do
+        try{
+            if(conn!=null)
+            conn.close();
+        }catch(SQLException se){
+            se.printStackTrace();
+        }
+   }
+
+    }
+    
+    
+    
 }
